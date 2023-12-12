14127dd61ee368c797b5bb52d80f5bf310064fee
Will Redding <willredding3.14@gmail.com>
2023-12-12 17:26:20 -0500

    add algorithms learning path

--- output_generator.py
+++ output_generator.py
@@ -42,7 +42,7 @@ class OutputGenerator():
                 results.append(link)
                 self.addPathways(link, connections, results)
         self.display(results)
-        self.write(results, "evaluation-learning-path-topology")
+        self.write(results, "learning-path-algorithms")
 
     def addPathways(self, topic, connections, results):
         outLinks = connections.get(topic)
