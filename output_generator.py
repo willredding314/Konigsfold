7d57024d74d1f28e45eb197c212419fe6399fdd6
Will Redding <willredding3.14@gmail.com>
2023-12-12 21:25:42 -0500

    Add heat transfer learning path

--- output_generator.py
+++ output_generator.py
@@ -42,7 +42,7 @@ class OutputGenerator():
                 results.append(link)
                 self.addPathways(link, connections, results)
         self.display(results)
-        self.write(results, "learning-path-graph-theory")
+        self.write(results, "learning-path-heat-transfer")
 
     def addPathways(self, topic, connections, results):
         outLinks = connections.get(topic)
